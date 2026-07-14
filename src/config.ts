4f77a3a8d4dd5f910d890a7089deb6a8e7bebd86
Nick Melnick <nmelnick@users.noreply.github.com>
2026-07-14 11:29:00 -0400

    Make activity log to file optional, show log entries before UI is rendered (#8)

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,13 @@ const CONFIG_PATH = path.resolve(process.cwd(), "sixtyeight.config.json");
 interface ConfigFile {
   serialPort: string;
   ignoreCapabilityDimming: boolean;
+  writeActivityLogToFile: boolean;
 }
 
 export class Config {
   public static serialPort: string = "/dev/ttyUSB0";
   public static ignoreCapabilityDimming: boolean = false;
+  public static writeActivityLogToFile: boolean = false;
 
   public static exists(): boolean {
     return fs.existsSync(CONFIG_PATH);
@@ -31,12 +33,16 @@ export class Config {
     if (parsed?.ignoreCapabilityDimming !== undefined) {
       Config.ignoreCapabilityDimming = parsed.ignoreCapabilityDimming;
     }
+    if (parsed?.writeActivityLogToFile !== undefined) {
+      Config.writeActivityLogToFile = parsed.writeActivityLogToFile;
+    }
   }
 
   public static save(): void {
     const data: ConfigFile = {
       serialPort: Config.serialPort,
       ignoreCapabilityDimming: Config.ignoreCapabilityDimming,
+      writeActivityLogToFile: Config.writeActivityLogToFile,
     };
     fs.writeFileSync(
       CONFIG_PATH,
